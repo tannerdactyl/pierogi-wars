c8b79f0c268a97a6549888d8301f6a0797b807b2
Tanner Hendrickson <tannerhendrickson2015@u.northwestern.edu>
2014-05-28 06:48:44 -0500

    hide main story at start

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
 	
 
+	$("#main-content").hide(); //Hides the main story to begin with
+
+	
+	// $(".story-link").click() {
+
+	// };
+	// function storychooser() {
+	// 	$("#city").show
+	// }
 
 
 
