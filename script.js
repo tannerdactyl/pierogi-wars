cde7356dc84c1a9a5404a49ef8f721e2d01081b8
Tanner Hendrickson <tannerhendrickson2015@u.northwestern.edu>
2014-05-28 12:01:58 -0500

    Story selection fully working!
    Now on to navigation, back buttons, etc

--- script.js
+++ script.js
@@ -6,31 +6,29 @@ $(document).ready(function() {
 	
 	$(".story-link").click(function() { 
 		var storyname = this.id;
-		storychooser(storyname);
+		$("#main-content").show();
+
+		if (storyname == "city-link") {
+			$("#truck, #restaurant, #full-photo-3, #full-photo-4").hide();
+			$("#city, #full-photo-2").show();
+			
+		};
+		if (storyname == "restaurant-link") {
+			$("#truck, #city, #full-photo-2, #full-photo-4").hide();
+			$("#restaurant, #full-photo-3").show();
+			
+		};
+		
+		if (storyname == "truck-link") {
+			$("#restaurant, #city, #full-photo-2, #full-photo-3").hide();
+			$("#truck, #full-photo-4").show();
+		};
 
 
 	});
 
 	
 	
-	function storychooser(storyname) {
-		console.log(storyname);
-		$("#main-content").show();
-
-		if (storyname == "restaurant-link") {
-			$("#restaurant").show();
-			$("#truck","#city").hide();
-		};
-		if (storyname == "city-link") {
-			$("#city").show();
-			$("#truck","#restaurant").hide();
-		};
-		
-		if (storyname == "truck-link") {
-			$("#truck").show();
-			$("#restaurant","#city").hide();
-		};
-	}
 
 
 
