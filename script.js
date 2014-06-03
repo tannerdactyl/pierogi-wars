84b2151cc04b318be57b2a3d28bff03f1fd3b4c1
Tanner Hendrickson <tannerhendrickson2015@u.northwestern.edu>
2014-06-03 15:46:42 -0500

    added button ids
    also, commented out old JS

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
 	
 
-	$("#main-content").hide(); 
+	// $("#main-content").hide(); 
 
 	
-	$(".story-link").click(function() { 
-		var storyname = this.id;
-		$("#main-content").show();
+	// $(".story-link").click(function() { 
+	// 	var storyname = this.id;
+	// 	$("#main-content").show();
 
-		if (storyname == "city-link") {
-			$("#truck, #restaurant, #full-photo-3, #full-photo-4").fadeOut("fast");
-			$("#city, #full-photo-2").fadeIn("slow");
+	// 	if (storyname == "city-link") {
+	// 		$("#truck, #restaurant, #full-photo-3, #full-photo-4").fadeOut("fast");
+	// 		$("#city, #full-photo-2").fadeIn("slow");
 			
-		};
-		if (storyname == "restaurant-link") {
-			$("#truck, #city, #full-photo-2, #full-photo-4").fadeOut("fast");
-			$("#restaurant, #full-photo-3").fadeIn("slow");
+	// 	};
+	// 	if (storyname == "restaurant-link") {
+	// 		$("#truck, #city, #full-photo-2, #full-photo-4").fadeOut("fast");
+	// 		$("#restaurant, #full-photo-3").fadeIn("slow");
 			
-		};
+	// 	};
 		
-		if (storyname == "truck-link") {
-			$("#restaurant, #city, #full-photo-2, #full-photo-3").fadeOut("fast");
-			$("#truck, #full-photo-4").fadeIn("slow");
-		};
+	// 	if (storyname == "truck-link") {
+	// 		$("#restaurant, #city, #full-photo-2, #full-photo-3").fadeOut("fast");
+	// 		$("#truck, #full-photo-4").fadeIn("slow");
+	// 	};
 
 
-	});
+	// });
 
 	
-	$(".back-link").click(function() {
-		$("#main-content").fadeOut("fast");
-	});
-
+	// $(".back-link").click(function() {
+	// 	$("#main-content").fadeOut("fast");
+	// });
+	
 
 
 
