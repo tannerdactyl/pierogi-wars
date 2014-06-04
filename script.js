d7de72e00b31694591274e005031da8aa4dd5be3
Tanner Hendrickson <tannerhendrickson2015@u.northwestern.edu>
2014-06-04 10:33:44 -0500

    Quick fix for button change
    I think we’re good!

--- script.js
+++ script.js
@@ -12,14 +12,41 @@ $(document).ready(function() {
 	
 	//Story navigation buttons
 	$("#city-control, #teaser-1").click(function() { 
+		// Turn button yellow and text black
+		$("#city-control").animate({backgroundColor: 'rgba(255, 213, 25, .5)'});
+		$("#control-1").animate({color: 'rgba(0, 0, 0, 1)'});
+		
+		// Make sure other buttons are pink and white
+		$("#restaurant-control, #truck-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
+		$("#control-2, #control-3").animate({color: 'rgba(255, 255, 255, 1)'});
+
 		scrollTo("#city");
 	});
 
 	$("#restaurant-control, #teaser-2").click(function() { 
+		// Turn button yellow and text black
+		$("#restaurant-control").animate({backgroundColor: 'rgba(255, 213, 25, .5)'});
+		$("#control-2").animate({color: 'rgba(0, 0, 0, 1)'});
+		
+		// Make sure other buttons are pink and white
+		$("#truck-control, #city-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
+		$("#control-3, #control-1").animate({color: 'rgba(255, 255, 255, 1)'});
+
 		scrollTo("#restaurant");
 	});
 
 	$("#truck-control, #teaser-3").click(function() { 
+		
+
+		// Quick hack to fix 3 button change
+		// Turn button yellow and text black
+		$("#truck-control").animate({backgroundColor: 'rgba(255, 213, 25, .5)'});
+		$("#control-3").animate({color: 'rgba(0, 0, 0, 1)'});
+		
+		// Make sure other buttons are pink and white
+		$("#restaurant-control, #city-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
+		$("#control-2, #control-1").animate({color: 'rgba(255, 255, 255, 1)'});
+
 		scrollTo("#truck");
 	});
 
@@ -90,15 +117,6 @@ $(document).ready(function() {
 			}
 		});
 
-	// $("#truck").waypoint(function() {
-	// 	// Turn button yellow and text black
-	// 	$("#truck-control").animate({backgroundColor: 'rgba(255, 213, 25, .5)'});
-	// 	$("#control-3").animate({color: 'rgba(0, 0, 0, 1)'});
-		
-	// 	// Make sure other buttons are pink and white
-	// 	$("#restaurant-control, #city-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
-	// 	$("#control-2, #control-1").animate({color: 'rgba(255, 255, 255, 1)'});
-	// });
 	
 	
 	
