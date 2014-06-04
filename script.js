9dd84ed20bfc9fe4f61a168f16f08fc34a1c417e
Tanner Hendrickson <tannerhendrickson2015@u.northwestern.edu>
2014-06-04 01:06:33 -0500

    still screwing around with color changes

--- script.js
+++ script.js
@@ -28,10 +28,10 @@ $(document).ready(function() {
 	$("#main-content").waypoint(function(direction) {
 		if (direction == "down") {
 			$("#controls").fadeIn("slow");
-		};
+		}
 		if (direction == "up") {
 			$("#controls").fadeOut("slow");
-		};
+		}
 	});
 
 	$("#city").waypoint(function() {
@@ -42,19 +42,34 @@ $(document).ready(function() {
 	});
 
 
-	$("#restaurant").waypoint(function() {
-		$("#restaurant-control").animate({backgroundColor: 'rgba(255, 213, 25, .5)'});
-		$("#city-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
-		$("#truck-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
+	$("#restaurant").waypoint(function(direction) {
+		if (direction == "down") {
+			$("#restaurant-control").animate({backgroundColor: 'rgba(255, 213, 25, .5)'});
+			$("#city-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
+			$("#truck-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
+		}
+		
+		if (direction == "up") {
+			$("#city-control").animate({backgroundColor: 'rgba(255, 213, 25, .5)'});
+			$("#restaurant-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
+			$("#truck-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
+		}
 
 	});
 
 
-	$("#truck").waypoint(function() {
-		$("#truck-control").animate({backgroundColor: 'rgba(255, 213, 25, .5)'});
-		$("#restaurant-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
-		$("#city-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
-
+	$("#truck").waypoint(function(direction) {
+		if (direction == "down") {
+			$("#truck-control").animate({backgroundColor: 'rgba(255, 213, 25, .5)'});
+			$("#city-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
+			$("#restaurant-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
+		}
+		
+		if (direction == "up") {
+			$("#restaurant-control").animate({backgroundColor: 'rgba(255, 213, 25, .5)'});
+			$("#city-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
+			$("#truck-control").animate({backgroundColor: 'rgba(255, 0 , 113, 1)'});
+		}
 	});
 	
 
